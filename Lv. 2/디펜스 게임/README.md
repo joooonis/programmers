643e57c9418812f16722ee8c088fd5c3af8d9dd6
jooooonis <xowns3213@naver.com>
2023-02-20 20:01:12 +0900

    Lv. 2 : 디펜스 게임

--- Lv. 2/디펜스 게임/README.md
+++ Lv. 2/디펜스 게임/README.md
@@ -1,8 +1,17 @@
 # 문제
 
-[시소짝꿍](https://school.programmers.co.kr/learn/courses/30/lessons/142085#)
+[디펜스 게임](https://school.programmers.co.kr/learn/courses/30/lessons/142085)
+
+# 풀이
+
+PriorityQueue(우선 순위 큐)를 사용합니다.
+
+1. 막힐 때까지 게임을 진행시킵니다. 막힐 때까지 가는 동안 적이 많았던 개수 순으로 PriorityQueue에 넣습니다.
+2. 막힌 경우 무적권이 있다면 현재스테이지의 enemy 수와 PriorityQueue의 peek을 비교하여 더 큰걸로 무적권을 사용하는것으로 합니다.
+3. 만약 PriorityQueue가 비었다면 무적권을 사용합니다.
+4. 무적권이 없다면 게임을 종료합니다.
 
 # 결과
 
-정확성: 40.6
-합계: 40.6 / 100.0
+정확성: 81.3
+합계: 81.3 / 100.0
--- Lv. 2/디펜스 게임/solution.js
+++ Lv. 2/디펜스 게임/solution.js
@@ -1,20 +1,68 @@
-function solution(n, k, enemy) {
-  let answer = 0;
-  for (let i = 0; i < enemy.length; i++) {
-    target = enemy.slice(i, i + k);
-    if (k > 0 && Math.min(target) !== enemy[i]) {
-      k -= 1;
-    } else if (n >= enemy[i]) {
-      n -= enemy[i];
-    } else if (k > 0) {
-      k -= 1;
-    } else {
-      return answer;
-    }
-    answer += 1;
+class PriorityQueue {
+  constructor() {
+    this.queue = [];
+  }
 
-    if (n <= 0 && k <= 0) {
-      return answer;
+  enqueue(item, priority) {
+    const queueItem = { item, priority };
+    let added = false;
+    for (let i = 0; i < this.queue.length; i++) {
+      if (queueItem.priority > this.queue[i].priority) {
+        this.queue.splice(i, 0, queueItem);
+        added = true;
+        break;
+      }
+    }
+    if (!added) {
+      this.queue.push(queueItem);
+    }
+  }
+
+  dequeue() {
+    return this.queue.shift().item;
+  }
+
+  isEmpty() {
+    return this.queue.length === 0;
+  }
+
+  length() {
+    return this.queue.length;
+  }
+
+  peek() {
+    return this.queue[0].item;
+  }
+}
+
+function solution(n, k, enemy) {
+  const pq = new PriorityQueue();
+  let answer = 0;
+  while (enemy.length > 0) {
+    e = enemy.shift();
+    if (n >= e) {
+      n -= e;
+      pq.enqueue(e, e);
+      answer++;
+    } else {
+      if (pq.isEmpty() && k > 0) {
+        k--;
+        answer++;
+        continue;
+      }
+      if (k > 0) {
+        k--;
+        answer++;
+        max = pq.peek();
+        if (max > e) {
+          pq.dequeue();
+          pq.enqueue(e, e);
+          n += max;
+          n -= e;
+        }
+      } else {
+        return answer;
+      }
     }
   }
   return answer;
