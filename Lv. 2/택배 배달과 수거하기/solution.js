e23546a194c1ed3120ecc4bb0b71b9c314ab3373
jooooonis <xowns3213@naver.com>
2023-02-24 23:17:59 +0900

    Lv. 2 : 택배 배달과 수거하기

--- Lv. 2/택배 배달과 수거하기/solution.js
+++ Lv. 2/택배 배달과 수거하기/solution.js
@@ -26,3 +26,26 @@ function solution(cap, n, deliveries, pickups) {
 
   return answer;
 }
+
+
+function solution(cap, n, deliveries, pickups) {
+  let answer = 0;
+  let d = 0;
+  let p = 0;
+  for (var i = n - 1; i >= 0; i--) {
+    let cnt = 0;
+
+    d -= deliveries[i];
+    p -= pickups[i];
+
+    while (d < 0 || p < 0) {
+      d += cap;
+      p += cap;
+      cnt += 1;
+    }
+
+    answer += (i + 1) * 2 * cnt;
+  }
+
+  return answer;
+}
