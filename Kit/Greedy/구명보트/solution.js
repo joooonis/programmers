ac0a214095cf854f36ecc827f16350037e4dce9c
jooooonis <xowns3213@naver.com>
2023-03-04 00:38:19 +0900

    refactoring

--- Kit/Greedy/구명보트/solution.js
+++ Kit/Greedy/구명보트/solution.js
@@ -4,14 +4,11 @@ function solution(people, limit) {
 
   while (people.length > 0) {
     let p = people.pop();
-    let right = false;
     while (p < limit && people.length > 0) {
-      if (right && p + people[people.length - 1] <= limit) {
+      if (p + people[people.length - 1] <= limit) {
         p += people.pop();
-        right = false;
-      } else if (!right && p + people[0] <= limit) {
+      } else if (p + people[0] <= limit) {
         p += people.shift();
-        right = true;
       } else {
         break;
       }
