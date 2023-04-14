a9a2553832486c0d80e0498f800c70059c2bfb3a
jooooonis <xowns3213@naver.com>
2023-04-14 16:37:05 +0900

    fail : 순위검색

--- KAKAO/week9/순위검색/solution.js
+++ KAKAO/week9/순위검색/solution.js
@@ -21,5 +21,63 @@ function pass(info, query) {
 
 function solution(info, query) {
   const answer = [];
-  const map = new Map();
+  const hash = {};
+
+  info.forEach((i) => {
+    const [language, work, career, fav, score] = i.split(' ');
+    const keys = [];
+    for (let i = 0; i < 16; i++) {
+      keys.push(
+        (i & 1 ? language : '-') +
+          (i & 2 ? work : '-') +
+          (i & 4 ? career : '-') +
+          (i & 8 ? fav : '-')
+      );
+    }
+    keys.forEach((k) => {
+      if (!hash[k]) hash[k] = [];
+      hash[k].push(Number(score));
+    });
+  });
+
+  for (let q of query) {
+    const [l, w, c, fs] = q.split(' and ');
+    const [f, s] = fs.split(' ');
+    const key = l + w + c + f;
+    if (!hash[key]) answer.push(0);
+    else {
+      const scores = hash[key];
+      scores.sort((a, b) => a - b);
+      let start = 0;
+      let end = scores.length;
+      while (start < end) {
+        const mid = Math.floor((start + end) / 2);
+        if (scores[mid] >= Number(s)) end = mid;
+        else start = mid + 1;
+      }
+      answer.push(scores.length - start);
+    }
+  }
+
+  return answer;
 }
+
+solution(
+  [
+    'java backend junior pizza 150',
+    'python frontend senior chicken 210',
+    'python frontend senior chicken 150',
+
+    'cpp backend senior pizza 260',
+    'java backend junior chicken 80',
+    'python backend senior chicken 50',
+  ],
+  [
+    'java and backend and junior and pizza 100',
+    'python and frontend and senior and chicken 200',
+    'cpp and - and senior and pizza 250',
+    '- and backend and senior and - 150',
+    '- and - and - and chicken 100',
+    '- and - and - and - 150',
+  ]
+);
