c18471470cd73acad936f9129fba0cbe69fa16c8
jooooonis <xowns3213@naver.com>
2023-04-21 14:20:59 +0900

    Lv. 2 : 마법의 엘리베이터

--- KAKAO/week11/마법의 엘리베이터.js
+++ KAKAO/week11/마법의 엘리베이터.js
@@ -4,22 +4,20 @@ function solution(storey) {
   // p=1-4이면 아래로 이동
   // p=6-10층이면 위로 이동
   // p=5이면 십의자리가 5이상이면 위로, 5미만이면 아래로 이동
+
   while (storey > 0) {
-    const p = storey % 10;
+    let p = storey % 10;
     if (p > 5) {
       answer += 10 - p;
       storey += 10;
-    } else if (
-      p == 5 &&
-      (storey + '').length >= 2 &&
-      (storey + '')[(storey + '').length - 2] >= 5
-    ) {
+    } else if (p === 5 && (storey + '')[(storey + '').length - 2] >= 5) {
       answer += 10 - p;
       storey += 10;
-    } else if (p > 0) {
+    } else {
       answer += p;
     }
     storey = Math.floor(storey / 10);
   }
+
   return answer;
 }
